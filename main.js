69feeafaf7ca44f1d913307c69554db51e1f7e79
Sebastian Pulido <jsalfonsop03@gmail.com>
2022-09-22 09:46:20 -0500

    First Version Finance Web

---
+++ main.js
@@ -0,0 +1,11 @@
+function myAnimation(){
+    const animate =  document.getElementById("singup");
+    animate.classList.add = "animate__heartBeat";
+    animate.style.background = "#816ae2";
+    animate.style.animation = "heartBeat";
+    animate.style.animationDuration = "1200ms";
+}
+
+
+
+
